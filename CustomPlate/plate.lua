d0ec1f7486518a1369233c0d9095017c2b612201
ValenciaModifcations <122960130+ValenciaModifcations@users.noreply.github.com>
2023-01-21 13:16:55 -0500

    Update plate.lua

--- CustomPlate/plate.lua
+++ CustomPlate/plate.lua
@@ -1,20 +1,57 @@
-local plateNames = {"plate01", "plate02"}
-local textureUrls = {"https://imgur.com/oAXDDBv.png", "https://imgur.com/QOdPjDv.png"} -- Upload your images here. Doesn't need to be imgur.
+--- Created by Shawn V, Valencia Modifcations. Please do not reupload without permission. ---
+
+
+local plateNames = {"plate01", "plate02", "plate03", "plate04", "plate05"}
+local textureUrls = {"https://imgur.com/oAXDDBv.png", "https://imgur.com/oAXDDBv.png", "https://imgur.com/oAXDDBv.png", "https://imgur.com/oAXDDBv.png", "https://imgur.com/dIORaay.png"} -- Upload your images here. Doesn't need to be imgur.
 local textureDic = CreateRuntimeTxd('duiTxd')
 
-for i = 1, #plateNames do
-    local object = CreateDui(textureUrls[i], 540, 300)
-    local handle = GetDuiHandle(object)
-    CreateRuntimeTextureFromDuiHandle(textureDic, "duiTex" .. i, handle)
-    AddReplaceTexture('vehshare', plateNames[i], 'duiTxd', "duiTex" .. i)
+if textureDic == nil then 
+    print("Error: Failed to create runtime texture dictionary 'duiTxd'")
+else
+    print("Runtime texture dictionary 'duiTxd' created successfully")
 end
-
-local object = CreateDui('https://i.imgur.com/Q3uw6V7.png', 540, 300) -- this URL doesn't need to be edited, its just the 2d model for the plate -- Load image into object
-local handle = GetDuiHandle(object) -- Gets DUI handle from object
-CreateRuntimeTextureFromDuiHandle(textureDic, "duiTex2", handle) -- Creates the texture "duiTex" in the "duiTxd" dictionary
-AddReplaceTexture('vehshare', 'plate01_n', 'duiTxd', 'duiTex2') -- Applies "duiTex2" from "duiTxd" to "plate01_n" from "vehshare"
-AddReplaceTexture('vehshare', 'plate02_n', 'duiTxd', 'duiTex2') -- Applies "duiTex2" from "duiTxd" to "plate02_n" from "vehshare"
-AddReplaceTexture('vehshare', 'plate03_n', 'duiTxd', 'duiTex2') -- Applies "duiTex2" from "duiTxd" to "plate03_n" from "vehshare"
-AddReplaceTexture('vehshare', 'plate04_n', 'duiTxd', 'duiTex2') -- Applies "duiTex2" from "duiTxd" to "plate04_n" from "vehshare"
-AddReplaceTexture('vehshare', 'plate05_n', 'duiTxd', 'duiTex2') -- Applies "duiTex2" from "duiTxd" to "plate05_n" from "vehshare"
-
+for i = 1, #plateNames do
+    local object = CreateDui(textureUrls[i], 1200, 700)
+    if object == nil then
+        print("Error: Failed to create DUI object for texture ".. textureUrls[i])
+    else
+        print("DUI object for texture ".. textureUrls[i].." created successfully")
+        local handle = GetDuiHandle(object)
+        if handle == nil then
+            print("Error: Failed to get DUI handle for object")
+        else
+            print("DUI handle for object obtained successfully")
+            if CreateRuntimeTextureFromDuiHandle(textureDic, "duiTex" .. i, handle) then
+                print("Runtime texture 'duiTex"..i.."' created successfully")
+                AddReplaceTexture('vehshare', plateNames[i], 'duiTxd', "duiTex" .. i)
+            else
+            print("Error: Failed to create runtime texture 'duiTex"..i.."'")
+             end
+        end
+    end
+end
+            
+            local object = CreateDui('https://i.imgur.com/Q3uw6V7.png', 1200, 700)
+            if object == nil then
+            print("Error: Failed to create DUI object for texture https://i.imgur.com/Q3uw6V7.png")
+            else
+            print("DUI object for texture https://i.imgur.com/Q3uw6V7.png created successfully")
+            local handle = GetDuiHandle(object)
+            if handle == nil then
+            print("Error: Failed to get DUI handle for object")
+            else
+            print("DUI handle for object obtained successfully")
+            if CreateRuntimeTextureFromDuiHandle(textureDic, "duiTex2", handle) then
+            print("Runtime texture 'duiTex2' created successfully")
+            AddReplaceTexture('vehshare', 'plate01_n', 'duiTxd', 'duiTex2')
+            AddReplaceTexture('vehshare', 'plate02_n', 'duiTxd', 'duiTex2')
+            AddReplaceTexture('vehshare', 'plate03_n', 'duiTxd', 'duiTex2')
+            AddReplaceTexture('vehshare', 'plate04_n', 'duiTxd','duiTex2')
+            AddReplaceTexture('vehshare', 'plate05_n', 'duiTxd', 'duiTex2')
+            else
+            print("Error: Failed to create runtime texture 'duiTex2'")
+        end
+    end
+end
+            
+            
